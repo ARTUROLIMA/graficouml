27ed41b628608dd2996c0764b2aaf36c6eccb3ae
ARTUR <LAPTOP@>
2020-07-18 19:07:08 -0500

    PruebaVariable

---
+++ PruebaVariable/src/PruebaVariable.java
@@ -0,0 +1,15 @@
+
+public class PruebaVariable {
+
+	public static void main(String[] args) {
+		boolean estaEncendido = true;
+		// if(estaEncendido == true)
+		if(estaEncendido) {
+			System.out.println("La tv esta encenida");
+		} else {
+			System.out.println("La tv esta apagada");
+		}
+
+	}
+
+}
